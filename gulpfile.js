7bf4f5d153c302190ab4a269e8b3896fa6c4a371
bombenko <sashazabn@mail.ru>
2017-01-18 23:34:25 +0300

    Исправлен logo, добавил файл типографики, подключил автопрефиксер и исправил другие недочеты 1. Убрал лишнюю вложенность в блоке logo - заменил div на h1 2. Создал файл typographics.less, содержащий типографику 3. Подключил автопрефиксер 4. Сократил значения ширины в блоках до 5 знаков после запятой 5. Убрал тег br в блоке logo

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require("gulp"),
 less = require("gulp-less"),
+autoprefix = require("less-plugin-autoprefix"),
+autoprefixPlugin = new autoprefix({browsers: ["last 2 versions"]}),
 
 devDist = "dev/",
 releaseDist = "release/";
 
 gulp.task("less", function(){
     gulp.src(devDist + "css/style.less")
-    .pipe(less())
+    .pipe(less({
+        plugins: [autoprefixPlugin]
+    }))
     .pipe(gulp.dest(devDist + "css/"));
 })
 
